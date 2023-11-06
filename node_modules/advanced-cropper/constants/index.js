8e1e2094f2d872008b87cde7474a0d9e7238bfa7
conghuynhho <conghuynhho12@gmail.com>
2023-11-06 18:21:48 +0700

    main remove node_modules

--- node_modules/advanced-cropper/constants/index.js
+++
@@ -1,5 +0,0 @@
-var ALL_DIRECTIONS = ['left', 'right', 'top', 'bottom'];
-var HORIZONTAL_DIRECTIONS = ['left', 'right'];
-var VERTICAL_DIRECTIONS = ['top', 'bottom'];
-
-export { ALL_DIRECTIONS, HORIZONTAL_DIRECTIONS, VERTICAL_DIRECTIONS };
